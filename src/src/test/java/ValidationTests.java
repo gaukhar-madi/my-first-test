ff352e4d3cb7a011e04569a7acd7eb4599471b21
Gauhar Madi <madikgau@gmail.com>
2026-08-05 00:04:10 +0500

    Homework Fill Form Happy Test

--- src/src/test/java/ValidationTests.java
+++ src/src/test/java/ValidationTests.java
@@ -1,28 +1,17 @@
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.cssValue;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.*;
 
-public class ValidationTests {
+public class ValidationTests extends TestBase {
 
-
-    @BeforeEach
-    void setUp(){
-
-        Configuration.browserSize = "1920x1080";
-
-        open("https://demoqa.com/automation-practice-form");
-    }
 
     @Test
-    void EmptyLastNameTest(){
-
-        // LEAVE LAST NAME EMPTY
+    void emptyLastNameTest(){
 
         // заполнить имя
         $("[id=firstName]").setValue("Tom");
@@ -30,15 +19,7 @@ public class ValidationTests {
         // CLICK SUBMIT BUTTON
 
         SelenideElement button = $("#submit");
-
-        // прокруить страницу пока не будет видна кнопка
-        executeJavaScript(
-                "arguments[0].scrollIntoView({block: 'center'});",
-                button
-        );
-
-        // кликнуть на кнопку Submit
-        executeJavaScript("arguments[0].click();", button);
+        button.click();
 
         // проверить класс валидации отображается
         boolean displayed = $(byClassName("was-validated")).isDisplayed();
@@ -46,13 +27,12 @@ public class ValidationTests {
 
         // проверить что поля формы красные
         $("#lastName").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $("#userForm").shouldHave(cssClass("was-validated"));
 
 
     }
     @Test
-    void WrongEmailForm(){
-
-        // ВВЕСТИ НЕВЕРНЫЙ email
+    void wrongEmailForm(){
 
         // вводим email без точки и доменной зоны
         $("[id=userEmail]").setValue("michaelthomas@mail");
@@ -60,44 +40,26 @@ public class ValidationTests {
         // CLICK SUBMIT BUTTON
 
         SelenideElement button = $("#submit");
-
-        // прокруить страницу пока не будет видна кнопка
-        executeJavaScript(
-                "arguments[0].scrollIntoView({block: 'center'});",
-                button
-        );
-
-        // кликнуть на кнопку Submit
-        executeJavaScript("arguments[0].click();", button);
-
+        button.click();
         // проверить класс валидации отображается
         boolean displayed = $(byClassName("was-validated")).isDisplayed();
         Assertions.assertTrue(displayed);
 
         // проверить что поля формы красные
         $("#userEmail").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $("#userForm").shouldHave(cssClass("was-validated"));
     }
 
     @Test
-    void LongMobileNumber(){
+    void longMobileNumber(){
 
-        // ВВЕСТИ ДЛИННЫЙ НОМЕР (МЕНЬШЕ 10 ЦИФР)
 
         // вводим номер телефона < 10 цифр
         $("[id=userNumber]").setValue("777000555");
 
-        // CLICK SUBMIT BUTTON
-
+        // Submit
         SelenideElement button = $("#submit");
-
-        // прокруить страницу пока не будет видна кнопка
-        executeJavaScript(
-                "arguments[0].scrollIntoView({block: 'center'});",
-                button
-        );
-
-        // кликнуть на кнопку Submit
-        executeJavaScript("arguments[0].click();", button);
+        button.click();
 
         // проверить класс валидации отображается
         boolean displayed = $(byClassName("was-validated")).isDisplayed();
@@ -105,6 +67,7 @@ public class ValidationTests {
 
         // проверить что поля формы красные
         $("#userNumber").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $("#userForm").shouldHave(cssClass("was-validated"));
 
     }
 
