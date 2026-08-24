24e2713daa68e7f568080a54eaaf396f4ab82843
Gauhar Madi <madikgau@gmail.com>
2026-08-24 13:23:37 +0500

    Homework Added Parameters to run in Jenkins

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,32 +3,44 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebDriverConfig;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import helpers.Attach;
-
 
 import java.util.Map;
 
 
 public class TestBase {
 
+    private static final WebDriverConfig config =
+            ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+
     @BeforeAll
     public static void beforeAll() {
 
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = config.baseUrl();
+        Configuration.browser = config.browser();
+        Configuration.browserSize = config.browserSize();
+        Configuration.headless = config.headless();
         Configuration.timeout = 10000;
         Configuration.pageLoadStrategy = "eager";
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        Configuration.browserCapabilities.setCapability("selenoid:options", Map.of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
+        if (!config.browserVersion().isBlank()) {
+            Configuration.browserVersion = config.browserVersion();
+        }
+
+        // Если remoteUrl задан -> запускаемся на удалённом Selenoid, иначе локально
+        if (!config.remoteUrl().isBlank()) {
+            Configuration.remote = config.remoteUrl();
+            Configuration.browserCapabilities.setCapability("selenoid:options", Map.of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+        }
     }
 
     @BeforeEach
